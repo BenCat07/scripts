e4020c3f76eca59f987034a108d1d3368a7915d7
LightCat <BenCat07@gmail.com>
2019-01-27 11:00:12 +0100

    Improve and add notice

--- clipboard.sh
+++ clipboard.sh
@@ -1,8 +1,9 @@
 # Removes Windows line endings from clipboard. Intended to be run in background.
 # Prevents Chrome, Discord and other apps from freezing/crashing when pasting
 # text from Team Fortress 2 console
+# sadly breaks images on clipboard
 
 while true; do
-	xclip -selection clipboard -o | tr -d '\015' | xclip -selection clipboard -i
+	xclip -selection clipboard -o | xclip -selection clipboard -i
 	sleep 1
 done
